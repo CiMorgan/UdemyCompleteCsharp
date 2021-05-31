c73b19611c6db05e0498c5f76609b9870b128260
CiMorgan <camorgan70@gmail.com>
2021-05-31 16:29:54 -0400

    Variables

--- UdemyCompleteCsharp4/Program.cs
+++ UdemyCompleteCsharp4/Program.cs
@@ -4,9 +4,23 @@ namespace UdemyCompleteCsharp4
 {
     class Program
     {
-        static void Main()   //Main method
+        //static void Main()   //Main method - where C# programs start
+        //{
+        //    System.Diagnostics.Debug.WriteLine("Hello World");
+        //}
+
+        public static void Main(String[] args)   //allow Main() method to accept command line arguments
         {
-            System.Diagnostics.Debug.WriteLine("Hello World");  
+            int x = 5;
+            float y = 3.14f;
+            string strX = "Hello World!";
+            char charX = 'a';
+            bool boolX = false;
+            System.Diagnostics.Debug.WriteLine(x);
+            System.Diagnostics.Debug.WriteLine(y);
+            System.Diagnostics.Debug.WriteLine(strX);
+            System.Diagnostics.Debug.WriteLine(charX);
+            System.Diagnostics.Debug.WriteLine(boolX);
         }
     }
 }
