a6718f34645b95ad31ffcd9f8868e9b32be0d27a
CiMorgan <camorgan70@gmail.com>
2021-05-31 16:12:39 -0400

    Initial Commit

---
+++ UdemyCompleteCsharp4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyCompleteCsharp4
+{
+    class Program
+    {
+        static void Main()   //Main method
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World");  
+        }
+    }
+}
