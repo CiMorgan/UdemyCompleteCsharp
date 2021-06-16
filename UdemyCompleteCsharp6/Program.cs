23cff5af0890acee49f8d95f64d5cbffb9038a04
CiMorgan <camorgan70@gmail.com>
2021-06-16 13:05:34 -0400

    DateTime

---
+++ UdemyCompleteCsharp6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UdemyCompleteCsharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime();
+            date = DateTime.Now;
+            Console.WriteLine(date);
+        }
+    }
+}
