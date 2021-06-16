123f06dd66f6b2c1be9b8bc1efb944afd2ab0d89
CiMorgan <camorgan70@gmail.com>
2021-06-16 13:32:35 -0400

    DateTime Format

--- UdemyCompleteCsharp6/Program.cs
+++ UdemyCompleteCsharp6/Program.cs
@@ -14,6 +14,11 @@ namespace UdemyCompleteCsharp6
             Console.WriteLine(span.Minutes);
             Console.WriteLine(span.Seconds);
             Console.WriteLine(span.Milliseconds);
+            Console.WriteLine(date.ToString("MM/dd/yyyy"));
+            Console.WriteLine(date.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(date.ToString("dddd, MMMM dd, yyyy"));
+            Console.WriteLine(date.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            Console.WriteLine(date.ToString("MM/dd/yyyy  hh:mm tt"));
         }
     }
 }
