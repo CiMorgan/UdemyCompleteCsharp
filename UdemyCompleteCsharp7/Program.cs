76c5e20e8bb09205cf6d6c92b6e784ed18bf6673
CiMorgan <camorgan70@gmail.com>
2021-06-17 21:48:56 -0400

    if elseif else

--- UdemyCompleteCsharp7/Program.cs
+++ UdemyCompleteCsharp7/Program.cs
@@ -6,7 +6,21 @@ namespace UdemyCompleteCsharp7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int m = 5;
+            int n = 5;
+            if (m < n)
+            {
+                Console.WriteLine("m is less than n");
+            }
+            else if (m > n)
+            {
+                Console.WriteLine("m is greater than n");
+            }
+            else
+            {
+                Console.WriteLine("m is equal to n");
+            }
+            
         }
     }
 }
