fbcf12a9f472c7a0639250ce2abb6f89f7d7d3cc
CiMorgan <camorgan70@gmail.com>
2021-06-18 23:00:09 -0400

    switch

--- UdemyCompleteCsharp7/Program.cs
+++ UdemyCompleteCsharp7/Program.cs
@@ -6,6 +6,7 @@ namespace UdemyCompleteCsharp7
     {
         static void Main(string[] args)
         {
+            //if else if else
             int m = 5;
             int n = 5;
             if (m < n)
@@ -21,6 +22,8 @@ namespace UdemyCompleteCsharp7
                 Console.WriteLine("m is equal to n");
             }
             
+            //switch
+
         }
     }
 }
