cc64d064188184a53ed46c3b4cc66f44fe1a8b68
CiMorgan <camorgan70@gmail.com>
2021-06-22 12:12:00 -0400

    Read

--- UdemyCompleteCsharp8/Program.cs
+++ UdemyCompleteCsharp8/Program.cs
@@ -7,8 +7,46 @@ namespace UdemyCompleteCsharp8
         static void Main(string[] args)
         {
             Console.WriteLine("Write you name");
-            string userInput = Console.ReadLine();
-            Console.WriteLine("My name is "+userInput);
+            string uInput = Console.ReadLine();
+            Console.WriteLine("My name is "+uInput);
+
+            Console.WriteLine("Hit a key: ");
+            int x = Console.Read();   //Read() returns an int
+            Console.WriteLine(x);
+
+            //change background of console based on key pressed
+            Console.WriteLine("g=green, r=red, b=blue, w=white");
+            int y = Console.Read();
+            char userInput = Convert.ToChar(y);
+            while (userInput != 'z')
+            {
+                switch (userInput)
+                {
+                    case 'g':
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        break;
+                    case 'r':
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        break;
+                    case 'b':
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        break;
+                    case 'w':
+                        Console.BackgroundColor = ConsoleColor.White;
+                        break;
+                    default:
+                        break;
+                }
+                Console.Clear();
+                Console.WriteLine("g=green, r=red, b=blue, w=white");
+
+                y = Console.Read();
+                userInput = Convert.ToChar(y);
+            }
+
+
+
+
         }
     }
 }
