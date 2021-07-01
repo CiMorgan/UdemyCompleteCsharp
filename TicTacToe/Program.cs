993a741620db20cfc334fbd0eceea2ebfe560e9d
CiMorgan <camorgan70@gmail.com>
2021-06-30 20:51:32 -0400

    TicTacToe

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -5,6 +5,27 @@ namespace TicTacToe
 {
     class Program
     {
+        static char[] spaces = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int flag;
+
+        /// <summary>
+        /// Draws the game board
+        /// </summary>
+        static void DrawBoard()
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}   |  {1}   |   {2}  ", spaces[0], spaces[1], spaces[2]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}   |  {1}   |   {2}  ", spaces[3], spaces[4], spaces[5]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}   |  {1}   |   {2}  ", spaces[6], spaces[7], spaces[8]);
+            Console.WriteLine("_____|_____|_____");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
