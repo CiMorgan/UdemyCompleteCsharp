18251b66ceb732c51c684f4ffb96e5898226be51
CiMorgan <camorgan70@gmail.com>
2021-07-05 21:11:46 -0400

    properties

--- UdemyCompleteCsharp9/Program.cs
+++ UdemyCompleteCsharp9/Program.cs
@@ -2,31 +2,53 @@
 
 namespace UdemyCompleteCsharp9
 {
-    public class Example
-    {
-        int exampleNumber;
-        string exampleName;
+    //public class Example
+    //{
+    //    int exampleNumber;
+    //    string exampleName;
 
-        public Example(int num)
-        {
-            exampleNumber = num;
-        }
+    //    public Example(int num)
+    //    {
+    //        exampleNumber = num;
+    //    }
 
-        public Example(int num, string name)
-        {
-            exampleNumber = num;
-            exampleName = name;
-        }
-        //finalizer used by garbage collector when class is destroyed
-        //~Example(){}
+    //    public Example(int num, string name)
+    //    {
+    //        exampleNumber = num;
+    //        exampleName = name;
+    //    }
+    //    //finalizer used by garbage collector when class is destroyed
+    //    //~Example(){}
     
+    //    public static void Main(string[] args)
+    //    {
+    //        Example ex = new Example(5);
+    //        Example ex2 = new Example(3, "John");
+    //        Console.WriteLine(ex.exampleNumber);
+    //        Console.WriteLine(ex2.exampleName+" has "+ex2.exampleNumber+" Bitcoins.");
+    //    }
+
+    //}
+    public class Program
+    {
+        //public string Id { get; set; } //autoimplemented property - get and set with no special syntax
+        private string id;
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
         public static void Main(string[] args)
         {
-            Example ex = new Example(5);
-            Example ex2 = new Example(3, "John");
-            Console.WriteLine(ex.exampleNumber);
-            Console.WriteLine(ex2.exampleName+" has "+ex2.exampleNumber+" Bitcoins.");
+            Program program = new Program();
+            program.Id = "1234";
+            Console.WriteLine(program.Id);
         }
-
     }
 }
