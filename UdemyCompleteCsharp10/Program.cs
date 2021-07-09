526567e88fdf5d65f0b59a95f1ba224515c424cb
CiMorgan <camorgan70@gmail.com>
2021-07-08 21:06:37 -0400

    interface

--- UdemyCompleteCsharp10/Program.cs
+++ UdemyCompleteCsharp10/Program.cs
@@ -8,5 +8,12 @@ namespace UdemyCompleteCsharp10
         {
             Console.WriteLine("Hello World!");
         }
+
+        interface IExample   //C# convention to begin interfaces with I
+        {
+            void Example();  //includes method signature but not method itself
+            int Example2(int n);
+            string Example3(string str1, string str2);
+        }    
     }
 }
