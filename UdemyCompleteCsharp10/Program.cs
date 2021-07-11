6fa63b073fcd5213b943a5e6963abc71fe6ef64d
CiMorgan <camorgan70@gmail.com>
2021-07-10 22:23:56 -0400

    INotifyProp

--- UdemyCompleteCsharp10/Program.cs
+++ UdemyCompleteCsharp10/Program.cs
@@ -1,35 +1,58 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace UdemyCompleteCsharp10
 {
-    class Program : IExample, ISecondExample  //need to use all methods in the interface otherwise it will not compile
+    //class Program : IExample, ISecondExample   //separate interfaces with commas
+    //{
+    //    static void Main(string[] args) //need to use all methods in the interface(s) otherwise it will not compile
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+
+    //    public void Example()
+    //    {
+    //        Console.WriteLine("Hello World");
+    //    }
+
+    //    public int Example2(int n)
+    //    {
+    //        return 1;
+    //    }
+
+    //    public string Example3(string str1, string str2)
+    //    {
+    //        return str1 + " " + str2;
+    //    }
+    //}
+    //interface IExample   //C# convention to begin interfaces with I
+    //{
+    //    void Example();  //includes method signature but not method itself
+    //    int Example2(int n);
+    //    string Example3(string str1, string str2);
+    //}
+
+    //interface ISecondExample { }
+
+    class Program2 : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged; //auto filled with Quick actions and refactoring
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
 
-        public void Example()
-        {
-            Console.WriteLine("Hello World");
-        }
-
-        public int Example2(int n)
-        {
-            return 1;
-        }
-
-        public string Example3(string str1, string str2)
-        {
-            return str1 + " " + str2;
         }
     }
-    interface IExample   //C# convention to begin interfaces with I
-    {
-        void Example();  //includes method signature but not method itself
-        int Example2(int n);
-        string Example3(string str1, string str2);
-    }
-
-    interface ISecondExample { }
 }
