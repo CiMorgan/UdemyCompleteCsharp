da52aea90c6a7faada5b56c357746be17e8c4b95
CiMorgan <camorgan70@gmail.com>
2021-07-12 21:40:07 -0400

    Abstract Method

--- UdemyCompleteCsharp10pt2/Program.cs
+++ UdemyCompleteCsharp10pt2/Program.cs
@@ -4,9 +4,18 @@ namespace UdemyCompleteCsharp10pt2
 {
     class Program
     {
-
+        
     }
-    abstract class Animal { }
+    abstract class Animal
+    {
+        public abstract void Run(int speed);
+    }
 
-    class Dog : Animal { }
+    class Dog : Animal 
+    {
+        public override void Run(int speed)
+        {
+            Console.WriteLine("The dog is running at speed: " + speed);
+        }
+    }
 }
