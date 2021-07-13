50549b69ba0fbf7252a2cba34c281eae7b4d19d1
CiMorgan <camorgan70@gmail.com>
2021-07-12 21:19:23 -0400

    IEnumerator

--- UdemyCompleteCsharp10/Program.cs
+++ UdemyCompleteCsharp10/Program.cs
@@ -149,54 +149,80 @@ namespace UdemyCompleteCsharp10
     //        return obj.Id.GetHashCode();
     //    }
     //}
-    class Wallet : IEnumerable
-    {
-        Money[] bills = null;
-        int openIndex = 0;   //next slot in wallet not taken up by a bill; keeps track of the next open index in the array
+    //class Wallet : IEnumerable
+    //{
+    //    Money[] bills = null;
+    //    int openIndex = 0;   //next slot in wallet not taken up by a bill; keeps track of the next open index in the array
 
-        public Wallet()
-        {
-            bills = new Money[100];
-        }
+    //    public Wallet()
+    //    {
+    //        bills = new Money[100];
+    //    }
 
-        public void Add(Money bill)
-        {
-            bills[openIndex] = bill;
-            openIndex++;
-        }
+    //    public void Add(Money bill)
+    //    {
+    //        bills[openIndex] = bill;
+    //        openIndex++;
+    //    }
 
-        public IEnumerator GetEnumerator()
-        {
-            foreach (Money bill in bills)
-            {
-                if (bill == null)
-                {
-                    break;
-                }
-                yield return bill;
-            }
-        }
-    }
-    class Money
+    //    public IEnumerator GetEnumerator()
+    //    {
+    //        foreach (Money bill in bills)
+    //        {
+    //            if (bill == null)
+    //            {
+    //                break;
+    //            }
+    //            yield return bill;
+    //        }
+    //    }
+    //}
+    //class Money
+    //{
+    //    public int amount;
+    //}
+    //class Program6
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        Wallet wallet = new Wallet();
+    //        wallet.Add(new Money() { amount = 1 });
+    //        wallet.Add(new Money() { amount = 5 });
+    //        wallet.Add(new Money() { amount = 10 });
+    //        wallet.Add(new Money() { amount = 20 });
+    //        wallet.Add(new Money() { amount = 50 });
+    //        wallet.Add(new Money() { amount = 100 });
+    //        foreach (Money money in wallet)
+    //        {
+    //            Console.WriteLine("Bill: " + money.amount);
+    //        }
+
+    //    }
+    //}
+
+    class Program7 : IEnumerator
     {
-        public int amount;
-    }
-    class Program6
-    {
+        List<object> items = new List<object>();
+        int current;
+        public object Current => items[current];
+
         public static void Main(string[] args)
         {
-            Wallet wallet = new Wallet();
-            wallet.Add(new Money() { amount = 1 });
-            wallet.Add(new Money() { amount = 5 });
-            wallet.Add(new Money() { amount = 10 });
-            wallet.Add(new Money() { amount = 20 });
-            wallet.Add(new Money() { amount = 50 });
-            wallet.Add(new Money() { amount = 100 });
-            foreach (Money money in wallet)
-            {
-                Console.WriteLine("Bill: " + money.amount);
-            }
 
+        }
+
+        public bool MoveNext()
+        {
+            if(items.Count == 0 || items.Count <= current)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            current = 0;
         }
     }
 }
