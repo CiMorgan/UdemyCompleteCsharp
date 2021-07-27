718cce1c5c56b4880f4ad2040b63b5a36f6a5f1b
CiMorgan <camorgan70@gmail.com>
2021-07-27 13:51:16 -0400

    arrays

--- UdemyCompleteCsharp11/Program.cs
+++ UdemyCompleteCsharp11/Program.cs
@@ -4,9 +4,17 @@ namespace UdemyCompleteCsharp11
 {
     class Program
     {
-        static void Main(string[] args)
+        //4 ways to create an array in C#
+        static string[] strings = new string[3];
+        static string[] strings1 = new string[3] { "dog", "cat", "elephant" };
+        static string[] strings2 = new string[] { "dog", "cat", "elephant" };
+        static string[] strings3 = { "dog", "cat", "elephant" }; //will need to use the new keyword if initialized later
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(strings[1]);
+            Console.WriteLine(strings1[1]);
+            Console.WriteLine(strings2[1]);
+            Console.WriteLine(strings3[1]);
         }
     }
 }
