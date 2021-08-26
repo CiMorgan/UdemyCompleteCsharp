614fb64c5c62682a060b727ad639f0f8a0fa1728
CiMorgan <camorgan70@gmail.com>
2021-08-25 20:14:22 -0400

    Build Tree

--- UdemyCompleteCsharp11/Program.cs
+++ UdemyCompleteCsharp11/Program.cs
@@ -461,53 +461,77 @@ namespace UdemyCompleteCsharp11
     //    }
     //}
 
-    public class Example //Trees
+    //Trees
+    class Example
     {
         public static void Main(string[] args)
         {
+            TreeNode<string> a = new TreeNode<string>("a");
+            TreeNode<string> b = new TreeNode<string>("b");
+            TreeNode<string> c = new TreeNode<string>("c");
+            //TreeNode<string> d = new TreeNode<string>("d");
+            //TreeNode<string> e = new TreeNode<string>("e");
+            //TreeNode<string> f = new TreeNode<string>("f");
+            //TreeNode<string> g = new TreeNode<string>("g");
 
+            a.SetLeft(b);
+            a.SetRight(c);
+            //b.SetLeft(d);
+            //c.SetLeft(e);
+            //e.SetLeft(f);
+            //e.SetRight(g);
+
+            Console.WriteLine(Program<string>.GetHeight(a));
         }
-
-        class TreeNode<T>  //T is a place holder for the type (int, string) of node
-        {
-            T value;
-            TreeNode<T> left = null;
-            TreeNode<T> right = null;
-
-            public TreeNode(T value)
-            {
-                this.value = value;
-            }
-            public TreeNode<T> GetLeft()
-            {
-                return left;
-            }
-            public TreeNode<T> GetRight()
-            {
-                return right;
-            }
-            public T GetValue()
-            {
-                return value;
-            }
-            public void SetValue(T value)
-            {
-                this.value=value;
-            }
-            public void SetRight(TreeNode<T> node)
-            {
-                right = node;
-            }
-            public void SetLeft(TreeNode<T> node)
-            {
-                left = node;
-            }
-
-
-        }
+        
     }
-        
-        
+    public class Program<T> 
+    {
+        public static int GetHeight(TreeNode<T> root)
+        {
+            if(root==null) { return 0; }
+            return Math.Max(GetHeight(root.GetLeft()), GetHeight(root.GetRight()) + 1);
+        }     
+    }
+    public class TreeNode<T>  //T is a place holder for the type (int, string) of node
+    {
+        T value;
+        TreeNode<T> left = null;
+        TreeNode<T> right = null;
+
+        public TreeNode(T value)
+        {
+            this.value = value;
+        }
+        public TreeNode<T> GetLeft()
+        {
+            return left;
+        }
+        public TreeNode<T> GetRight()
+        {
+            return right;
+        }
+        public T GetValue()
+        {
+            return value;
+        }
+        public void SetValue(T value)
+        {
+            this.value = value;
+        }
+        public void SetRight(TreeNode<T> node)
+        {
+            right = node;
+        }
+        public void SetLeft(TreeNode<T> node)
+        {
+            left = node;
+        }
+
+
+    }
+
+
 }
 
 
