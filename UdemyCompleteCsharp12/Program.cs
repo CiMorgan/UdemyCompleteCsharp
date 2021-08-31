6abf8293fe7275483b21a63439b96055867801ef
CiMorgan <camorgan70@gmail.com>
2021-08-30 20:16:51 -0400

    Adv

---
+++ UdemyCompleteCsharp12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyCompleteCsharp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
