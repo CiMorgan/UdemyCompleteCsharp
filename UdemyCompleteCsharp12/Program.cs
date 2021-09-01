c185e4068b92528b5d0722991a0a0fe03282df4e
CiMorgan <camorgan70@gmail.com>
2021-08-31 20:02:15 -0400

    actions

--- UdemyCompleteCsharp12/Program.cs
+++ UdemyCompleteCsharp12/Program.cs
@@ -20,34 +20,49 @@ namespace UdemyCompleteCsharp12
     //    }
     //}
 
+    //class Program
+    //{
+    //    private static event EventHandler evt;
+    //    //public static void HandleEvent(object sender, EventArgs evtArgs)
+    //    //{
+    //    //    Console.WriteLine("Hello World!"); 
+    //    //}
+    //    public static void Main(string[] args)
+    //    {
+    //        //evt += HandleEvent;
+    //        //evt.Invoke(null, new EventArgs());
+
+    //        //using lambda
+    //        evt += (sender, evtArgs) =>
+    //        {
+    //            Console.WriteLine("Hello World!");
+    //        };
+    //        evt += (sender, evtArgs) =>
+    //        {
+    //            Console.WriteLine("Goodbye World!");
+    //        };
+    //        evt.Invoke(null, new EventArgs());
+
+    //    }
+
+    //    //~Program()
+    //    //{
+    //    //    evt -= HandleEvent;
+    //    //}
+    //}
     class Program
     {
-        private static event EventHandler evt;
-        //public static void HandleEvent(object sender, EventArgs evtArgs)
-        //{
-        //    Console.WriteLine("Hello World!"); 
-        //}
-        public static void Main(string[] args)
+        private static Action<int, int> action;
+
+        static void HandleAction(int int1, int int2)
         {
-            //evt += HandleEvent;
-            //evt.Invoke(null, new EventArgs());
-
-            //using lambda
-            evt += (sender, evtArgs) =>
-            {
-                Console.WriteLine("Hello World!");
-            };
-            evt += (sender, evtArgs) =>
-            {
-                Console.WriteLine("Goodbye World!");
-            };
-            evt.Invoke(null, new EventArgs());
-
+            Console.WriteLine("Sum: "+ (int1+int2).ToString());
         }
 
-        //~Program()
-        //{
-        //    evt -= HandleEvent;
-        //}
+        static void Main(string[] args)
+        {
+            action += HandleAction;
+            action.Invoke(2, 3);
+        }
     }
 }
