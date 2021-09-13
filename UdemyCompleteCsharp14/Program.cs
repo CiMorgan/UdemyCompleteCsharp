b40714cee56a7770e7507b4051f72f94ace39ece
CiMorgan <camorgan70@gmail.com>
2021-09-13 19:06:06 -0400

    Namespaces

---
+++ UdemyCompleteCsharp14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyCompleteCsharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
