9685704d6c2a91a8ad0570f876663d7cc33c0ee0
CiMorgan <camorgan70@gmail.com>
2021-09-13 18:08:20 -0400

    REGEX

--- UdemyCompleteCsharp13/Program.cs
+++ UdemyCompleteCsharp13/Program.cs
@@ -2,6 +2,9 @@
 
 namespace UdemyCompleteCsharp13
 {
+    //REGEX - special text string that describes a search pattern
+    //used to perform find and replace with strings
+    //12 special char:   \  ^  $  .  |  ?  *  + (  )  [  ]
     class Program
     {
         static void Main(string[] args)
