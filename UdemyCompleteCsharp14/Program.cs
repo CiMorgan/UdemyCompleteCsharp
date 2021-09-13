733a57ad6fa86bba517060916fa90a3da78501ba
CiMorgan <camorgan70@gmail.com>
2021-09-13 19:24:03 -0400

    Namespaces

--- UdemyCompleteCsharp14/Program.cs
+++ UdemyCompleteCsharp14/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Log = System.Diagnostics.Debug;    //alias for longer namespace
+using Log = System.Diagnostics.Debug;    //alias for longer namespace; the dot is the dot operator 
+//dot operator (.) - one namespace or class is a part of another namespace or class
 
 namespace UdemyCompleteCsharp14
 {
